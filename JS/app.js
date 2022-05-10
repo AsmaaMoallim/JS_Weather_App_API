05423650b191fc4652ff0d909f85d1b2e0378de8
AsmaaMoallim <asmaaam1o1@gmail.com>
2022-05-10 14:59:43 +0300

    updating the UI

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,29 @@
-const cityForm = document.querySelector("Form");
+const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+
+const updateUI = (data) => {
+  const { cityDetails, weather } = data;
+
+  // update details
+  details.innerHTML = `<h5 class="my-3">${cityDetails.EnglishName}</h5>
+                <div class="my-3">${weather.WeatherText}</div>
+                <div class="display-4 my-4">
+                    <span>${weather.Temperature.Metric.Value}</span>
+                    <span>&deg;C</span>
+                </div>`;
+
+  if (card.classList.contains("d-none")){
+    card.classList.toggle("d-none");
+  } 
+};
+
+const updateCity = async (city) => {
+  const cityDetails = await getCity(city);
+  const weather = await getConditions(cityDetails.Key);
+  //   console.log(weather);
+  return { cityDetails, weather };
+};
 
 cityForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -9,13 +34,8 @@ cityForm.addEventListener("submit", (e) => {
 
   // updata the ui with new city
   updateCity(city)
-    .then((data) => console.log(data))
+    .then((data) => updateUI(data))
     .catch((err) => console.log(err));
 });
 
-const updateCity = async (city) => {
-  const cityDetails = await getCity(city);
-  const weather = await getConditions(cityDetails.Key);
-  //   console.log(weather);
-  return {cityDetails, weather};
-};
+ 
